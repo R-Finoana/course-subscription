96a216e8733944c2a27d2822f47c46082af9d7eb
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-05 20:25:48 +0300

    feat: implement CourseSubscriptionRequestedService to handle email sending

---
+++ src/main/java/com/school/hei/service/event/CourseSubscriptionRequestedService.java
@@ -0,0 +1,30 @@
+package com.school.hei.service.event;
+
+import com.school.hei.endpoint.event.model.CourseSubscriptionRequested;
+import com.school.hei.mail.Email;
+import com.school.hei.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+@Service
+@AllArgsConstructor
+public class CourseSubscriptionRequestedService implements Consumer<CourseSubscriptionRequested> {
+    private final Mailer mailer;
+
+    @SneakyThrows
+    @Override
+    public void accept(CourseSubscriptionRequested courseSubscription) {
+        InternetAddress recipientAddress = new InternetAddress(courseSubscription.getTo());
+        mailer.accept(new Email(
+                recipientAddress, List.of(), List.of(),
+                "Confirmation d'inscription",
+                "Vous êtes inscrits au cours: "+ courseSubscription.getCourseTitle(),
+                List.of()
+                ));
+    }
+}
