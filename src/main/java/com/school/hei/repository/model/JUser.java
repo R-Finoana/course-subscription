5e71ad16a3a7051b4dee00fad60ddaebe63db0cd
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-05 20:14:13 +0300

    fix: add id primary key to JUser

--- src/main/java/com/school/hei/repository/model/JUser.java
+++ src/main/java/com/school/hei/repository/model/JUser.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "user")
 public class JUser {
-  @GeneratedValue private UUID id;
+  @Id @GeneratedValue private UUID id;
 
   @Column(length = 200)
   private String first_name;
