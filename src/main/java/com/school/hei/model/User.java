1dd91fb8571cf88997cf45f83c94a4fea4356751
Lavorary <hei.lavorary@gmail.com>
2026-07-05 18:34:18 +0300

    feat: create User and Course entities, and JPA model (#2)
    * feat : create User and Course entity
    
    * feat : create JPA model for Course and User
    
    ---------
    
    Co-authored-by: Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>

---
+++ src/main/java/com/school/hei/model/User.java
@@ -0,0 +1,19 @@
+package com.school.hei.model;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.*;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@AllArgsConstructor
+@Builder
+public class User {
+  private UUID id_member;
+  private String first_name;
+  private String last_name;
+  private String username;
+  private String email;
+  private List<Course> courses;
+}
