d3ea4b4f400cfec8f1a917861c945e98f780ad59
Lavorary <hei.lavorary@gmail.com>
2026-07-06 08:34:50 +0300

    fix: change email type to String (#5)
    * fix : change email type to String
    
    ---------
    
    Co-authored-by: Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>

---
+++ src/main/java/com/school/hei/repositories/model/JCourse.java
@@ -0,0 +1,26 @@
+package com.school.hei.repositories.model;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "course")
+public class JCourse {
+  @Id @GeneratedValue private UUID id;
+
+  @Column(nullable = false, unique = true)
+  private String title;
+
+  @Column(nullable = false)
+  private Instant start;
+
+  @Column(nullable = false)
+  private Instant end;
+}
