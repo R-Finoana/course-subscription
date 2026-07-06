6a73b3d893c3dd681b237370c807a67049a0e2a4
Lavorary <hei.lavorary@gmail.com>
2026-07-06 08:28:04 +0300

    fix : change email type to String

--- src/main/java/com/school/hei/repositories/model/JUser.java
+++ src/main/java/com/school/hei/repositories/model/JUser.java
@@ -24,7 +24,7 @@ public class JUser {
   @Column(length = 200, unique = true)
   private String username;
 
-  @Enumerated(EnumType.STRING)
+  @Column(length = 200,  unique = true)
   private String email;
 
   @ManyToMany
