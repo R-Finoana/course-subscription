da4c5281ef80b59a8681442392f39c992c5ab0f8
Lavorary <hei.lavorary@gmail.com>
2026-07-06 09:08:46 +0300

    fix : change Subscription class to record

--- src/main/java/com/school/hei/model/Subscription.java
+++ src/main/java/com/school/hei/model/Subscription.java
@@ -1,14 +1,12 @@
 package com.school.hei.model;
 
 import com.school.hei.enums.SubscriptionStatusEnum;
+import lombok.*;
 
 import java.time.Instant;
 import java.util.UUID;
 
-public class Subscription {
-    private UUID id;
-    private Instant createdAt;
-    private SubscriptionStatusEnum status;
-    private UUID courseId;
-    private UUID userId;
-}
+@Builder
+public record Subscription(
+        UUID id, Instant createdAt, SubscriptionStatusEnum status, UUID courseId, UUID userId
+) {}
