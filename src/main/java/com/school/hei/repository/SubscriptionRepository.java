64c3146493c6e248c029648fd9769a4b363f8082
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-07 17:30:54 +0300

    feat: add find user and course by id in SubscriptionRepository

--- src/main/java/com/school/hei/repository/SubscriptionRepository.java
+++ src/main/java/com/school/hei/repository/SubscriptionRepository.java
@@ -4,4 +4,6 @@ import com.school.hei.repository.model.JSubscription;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubscriptionRepository extends JpaRepository<JSubscription, UUID> {}
+public interface SubscriptionRepository extends JpaRepository<JSubscription, UUID> {
+    boolean existsByUserIdAndCourseId(UUID userId, UUID courseId);
+}
